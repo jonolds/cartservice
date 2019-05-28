ef3cb1fa89c77659c4ecdffdcba45dc6d7eaa42c
jonolds <jonolds@gmail.com>
2019-05-28 11:19:51 -0500

    asdf

--- src/main/java/edu/uark/CartApplication.java
+++ src/main/java/edu/uark/CartApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RegisterApplication
+public class CartApplication
 {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RegisterApplication.class, args);
+		SpringApplication.run(CartApplication.class, args);
 	}
 }
