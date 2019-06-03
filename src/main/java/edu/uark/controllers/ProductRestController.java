a88c6947a765753479f09aaa36984d097c9ec8b8
jonolds <jonolds@gmail.com>
2019-06-03 03:36:06 -0500

    win

--- src/main/java/edu/uark/controllers/ProductRestController.java
+++ src/main/java/edu/uark/controllers/ProductRestController.java
@@ -19,6 +19,31 @@ import edu.uark.models.api.ProductListing;
 @RestController
 @RequestMapping(value = "/product")
 public class ProductRestController {
+
+	@RequestMapping(value = "/apiv0/{productId}", method = RequestMethod.GET)
+	public Product getProduct(@PathVariable UUID productId) {
+		System.out.println("REST product/apiv0/" + productId + "     getProduct(UUID " + productId + ")");
+		return (new ProductQuery()).setProductId(productId).execute();
+	}
+
+	@RequestMapping(value = "/apiv0/byLookupCode/{productLookupCode}", method = RequestMethod.GET)
+	public Product getProductByLookupCode(@PathVariable String productLookupCode) {
+		System.out.println("REST product/apiv0/byLookupCode" + productLookupCode + "     getProductByLookupCode(String " + productLookupCode + ")");
+		return (new ProductByLookupCodeQuery()).setLookupCode(productLookupCode).execute();
+	}
+
+	@RequestMapping(value = "/apiv0/products", method = RequestMethod.GET)
+	public ProductListing getProducts( ) {
+		System.out.println("REST product/apiv0/products      getProducts()");
+		return (new ProductsQuery()).execute();
+	}
+
+	@RequestMapping(value = "/apiv0/", method = RequestMethod.PUT)
+	public Product putActivity(@RequestBody Product product) {
+		System.out.println("REST product/apiv0/     putActivity(Product");
+		return (new ProductSaveCommand()).setApiProduct(product).execute();
+	}
+	
 	@ResponseBody
 	@RequestMapping(value = "/test2/{message}", method = RequestMethod.GET)
 	public String test2(@PathVariable String message) {
@@ -30,26 +55,5 @@ public class ProductRestController {
 	@RequestMapping(value = "/test", method = RequestMethod.GET)
 	public String test( ) {
 		return "Things are cool";
-	}
-
-	@RequestMapping(value = "/apiv0/{productId}", method = RequestMethod.GET)
-	public Product getProduct(@PathVariable UUID productId) {
-		System.out.println("rest address: product/apiv0/" + productId);
-		return (new ProductQuery()).setProductId(productId).execute();
-	}
-
-	@RequestMapping(value = "/apiv0/byLookupCode/{productLookupCode}", method = RequestMethod.GET)
-	public Product getProductByLookupCode(@PathVariable String productLookupCode) {
-		return (new ProductByLookupCodeQuery()).setLookupCode(productLookupCode).execute();
-	}
-
-	@RequestMapping(value = "/apiv0/products", method = RequestMethod.GET)
-	public ProductListing getProducts( ) {
-		return (new ProductsQuery()).execute();
-	}
-
-	@RequestMapping(value = "/apiv0/", method = RequestMethod.PUT)
-	public Product putActivity(@RequestBody Product product) {
-		return (new ProductSaveCommand()).setApiProduct(product).execute();
 	}
 }
